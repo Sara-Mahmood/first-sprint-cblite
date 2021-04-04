515c1fa6ba5f8407fcc119dba1fe11fdbef04f6f
Neha Jafry <51692007+Neha-Jafry@users.noreply.github.com>
2021-04-04 22:19:19 +0500

    Added angular app

---
+++ games/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SelectionComponent } from './selection/selection.component';
+
+const routes: Routes = [{path:'', component: SelectionComponent},
+                        {path:'selection', component: SelectionComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
